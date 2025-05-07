06aa5ddf9516a341120792d984d5eedf35482440
Pranav Solase <132485339+Pranavvv1000@users.noreply.github.com>
2025-05-07 19:44:44 +0530

    Update feature_main.py

--- feature_main.py
+++ feature_main.py
@@ -354,4 +354,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5004)
+    app.run(host='0.0.0.0', port=10001)
